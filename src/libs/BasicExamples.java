f28addcb6c13be667328ed38e24f32aee895b842
gencberke <berkegenc.dev@gmail.com>
2025-09-30 14:29:43 +0300

    feat: add examples of class relationships including composition, aggregation, and association

--- src/libs/BasicExamples.java
+++ src/libs/BasicExamples.java
@@ -329,10 +329,324 @@ class BasicExamples {
 //    }
 //}
 
+/*------------------ CLASS RELATIONSHIPS --------------------------------------------- CLASS RELATIONSHIPS ------------------------------- */
+
+// Composition, Aggregation, Association examples ----------------------
+/*
+package libs;
+
+import java.util.Arrays;
+
+class App {
+    public static void main(String[] args) {
+        DemoRaceApp.run();
+    }
+}
+
+class DemoRaceApp {
+    public static void run() {
+        Driver driver = new Driver();
+
+        driver.setName("Berke Genç");
+        driver.setRating(100);
+
+        Car car = new Car(driver);
+
+        car.run();
+
+        Pilot [] pilots = {new Pilot(1, 10000, "Berke Genç"), new Pilot(2, 50000, "Elif Çalışkan"),
+                new Pilot(3, 2500, "Emirhan Güroğlu")};
+        Plane plane = new Plane(4, pilots);
+
+        plane.fly();
+    }
+}
+
+class Plane {
+    private Engine [] m_engine;
+    private Pilot [] m_pilot;
+
+    public Plane(int n, Pilot[] m_pilot) {
+        m_engine = new Engine[n];
+        this.m_pilot = m_pilot;
+
+        for (int i = 0; i < n; ++i)
+            m_engine[i] = new Engine();
+    }
+
+    private void startEngines()
+    {
+        for (Engine e : m_engine)
+            e.startEngine();
+    }
+
+    private void accelerateEngines()
+    {
+        for (Engine e : m_engine)
+            e.accelerateEngine();
+    }
+
+    private void slowEngines()
+    {
+        for (Engine e : m_engine)
+            e.slowEngine();
+    }
+
+    private void stopEngines()
+    {
+        for (Engine e : m_engine)
+            e.stopEngine();
+    }
+
+    public void fly() {
+        for (Pilot pilot : m_pilot) {
+            System.out.printf("%s named pilot flys for %d duration with title of %d\n", pilot.getM_name(), pilot.getM_flightDuration(),
+                    pilot.getM_flightDuration());
 
 
+        }
+    }
+
+}
+
+class Car {
+    private final Engine m_engine;
+    private Driver m_driver;
+
+    public Car(Driver driver)
+    {
+        m_engine = new Engine();
+        m_driver = driver;
+    }
+
+    public Driver getDriver()
+    {
+        return m_driver;
+    }
+
+    public void setDriver(Driver driver)
+    {
+        m_driver = driver;
+    }
+
+    public void brake()
+    {
+        m_engine.slowEngine();
+    }
+
+    public void run()
+    {
+        System.out.printf("Driver:%s, %d%n", m_driver.getName(), m_driver.getRating());
+        m_engine.startEngine();
+        m_engine.accelerateEngine();
+
+        //...
+        System.out.println("running");
+
+        brake();
+        m_engine.stopEngine();
+    }
+
+}
+
+class Pilot {
+    private int m_title;
+    private int m_flightDuration;
+    private String m_name;
+
+    public Pilot(int m_title, int m_flightDuration, String m_name) {
+        this.m_title = m_title;
+        this.m_flightDuration = m_flightDuration;
+        this.m_name = m_name;
+    }
+
+    public int getM_title() {
+        return m_title;
+    }
+
+    public void setM_title(int m_title) {
+        this.m_title = m_title;
+    }
+
+    public int getM_flightDuration() {
+        return m_flightDuration;
+    }
+
+    public void setM_flightDuration(int m_flightDuration) {
+        this.m_flightDuration = m_flightDuration;
+    }
+
+    public String getM_name() {
+        return m_name;
+    }
+
+    public void setM_name(String m_name) {
+        this.m_name = m_name;
+    }
+
+}
+
+class Driver {
+    private String m_name;
+    private int m_rating;
+
+    //...
+
+    public String getName()
+    {
+        return m_name;
+    }
+
+    public void setName(String name)
+    {
+        m_name = name;
+    }
+
+    public int getRating()
+    {
+        return m_rating;
+    }
+
+    public void setRating(int rating)
+    {
+        m_rating = rating;
+    }
+
+}
 
 
+class Engine {
+    public void startEngine() {
+        System.out.println("engine started");
+    }
+
+    public void stopEngine() {
+        System.out.println("engine stopped");
+    }
+
+    public void slowEngine() {
+        System.out.println("engine slowed");
+    }
+
+    public void accelerateEngine() {
+        System.out.println("engine accelerated");
+    }
+
+}
+
+*/
+
+/*
+package libs;
+
+import java.util.Arrays;
+
+class App {
+    public static void main(String[] args) {
+        DemoTaxiApp.run();
+    }
+}
+
+class DemoTaxiApp {
+    public static void run() {
+        Driver driver = new Driver("Berke", 100);
+        Taxi taxi = new Taxi(driver, "34 ZBU 79");
+
+        Client client1 = new Client("elfclskn", "Elif Çalışkan");
+        Client client2 = new Client("pacii", "Emirhan Guroglu");
+
+        taxi.take(client1);
+        taxi.take(client2);
+    }
+}
+
+class Taxi {
+    private final String plate;
+    private Driver driver;
+
+    public Taxi(String plate) {
+        this.plate = plate;
+    }
+
+    public Taxi(Driver driver, String plate) {
+        this.driver = driver;
+        this.plate = plate;
+    }
+
+    public void take(Client client) {
+        System.out.printf("TAXİ: %s, DRIVER: %s, RATING: %d\n", this.plate, driver.getM_name(), driver.getM_rating());
+        System.out.printf("CLIENT: %s\n", client.getM_username());
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+}
+
+class Client {
+    private String m_username;
+    private String m_name;
+
+    public Client(String m_username, String m_name) {
+        this.m_username = m_username;
+        this.m_name = m_name;
+    }
+
+    public String getM_username() {
+        return m_username;
+    }
+
+    public void setM_username(String m_username) {
+        this.m_username = m_username;
+    }
+
+    public String getM_name() {
+        return m_name;
+    }
+
+    public void setM_name(String m_name) {
+        this.m_name = m_name;
+    }
+}
+
+class Driver {
+    private String m_name;
+    private int m_rating;
+
+    public Driver(String m_name, int m_rating) {
+        this.m_name = m_name;
+        this.m_rating = m_rating;
+    }
+
+    public Driver() {
+    }
+
+    public String getM_name() {
+        return m_name;
+    }
+
+    public void setM_name(String m_name) {
+        this.m_name = m_name;
+    }
+
+    public int getM_rating() {
+        return m_rating;
+    }
+
+    public void setM_rating(int m_rating) {
+        this.m_rating = m_rating;
+    }
+}
+ */
 
 
 
